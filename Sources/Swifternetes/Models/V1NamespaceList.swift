c27c62d53b4c537939167c9ed56f48017dfcbb0d
Steven Prichard <spprichard20@gmail.com>
2019-12-01 09:08:45 -0600

    remove optionality of kind in namespace list

--- Sources/Swifternetes/Models/V1NamespaceList.swift
+++ Sources/Swifternetes/Models/V1NamespaceList.swift
@@ -17,16 +17,14 @@ public struct V1NamespaceList: Codable {
     /** Items is the list of Namespace objects in the list. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/ */
     public var items: [V1Namespace]
     /** Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds */
-    public var kind: String?
+    public var kind: String
     public var metadata: V1ListMeta?
 
-    public init(apiVersion: String?, items: [V1Namespace], kind: String?, metadata: V1ListMeta?) {
-        self.apiVersion = apiVersion
-        self.items = items
-        self.kind = kind
-        self.metadata = metadata
-    }
-
-
+//    public init(apiVersion: String?, items: [V1Namespace], kind: String?, metadata: V1ListMeta?) {
+//        self.apiVersion = apiVersion
+//        self.items = items
+//        self.kind = kind
+//        self.metadata = metadata
+//    }
 }
 
